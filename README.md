22fd69744d39c4fe1285e175d327013594a8c253
Shvetank Dattatreya <hello.shvetank@gmail.com>
2025-08-02 21:23:45 +0530

    Renamed file and little change in code.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# onefinity
+# OneFinity
 A simple python based number guessing game.
+
+In this game, the program will decide on a number between the range 1 and 10 and you have to guess it. You will also get hints whether you guessed it lower or higher than the program's number.
+
+# Version 0.2
+Changed file name and made little changes to the code.
--- onefinity.py
+++ onefinity.py
@@ -1,15 +1,14 @@
 import random
 
 
-print("Welcome to this Number Guesser Game!")
+print("Welcome to OneFinity - A number guessing game!")
 print("To start playing press enter")
 input()
 
 print("Guess a number between 1 and 10")
 
 user_guess = int(input("Enter number: "))
-list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-computer_choice = random.choice(list_of_numbers)
+computer_choice = random.randrange(1, 10)
 
 while(user_guess != computer_choice):    
     if(user_guess > computer_choice):
